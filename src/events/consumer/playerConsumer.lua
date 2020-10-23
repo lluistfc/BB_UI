94c35f7562cf7e75cf0609ef0e13d0ae3e9fcc8e
Lluis Falco <lfalco@yaencontre.com>
2020-10-23 17:07:28 +0200

    remove commented code

--- src/events/consumer/playerConsumer.lua
+++ src/events/consumer/playerConsumer.lua
@@ -7,17 +7,7 @@ PlayerEventConsumer.AllowedEvents = {
 PlayerEventConsumer.Execute = function(self, event)
     
     if (self.AllowedEvents[event] == nil) then return end
+    
     PlayerHealth:BuildElements()
     PlayerPower:BuildElements()
-    --[[local healthConfig = BB_UI_Config[PlayerHealthConfig.unitType][PlayerHealthConfig.frameName] or PlayerHealthConfig
-    local powerConfig = BB_UI_Config[PlayerPowerConfig.unitType][PlayerPowerConfig.frameName] or PlayerPowerConfig
-
-    local PlayerHealth = BB_Frame(BB_Health)
-    local PlayerPower = BB_Frame(BB_Power)
-
-    PlayerHealth:Init(healthConfig)
-    PlayerPower:Init(powerConfig)
-
-    PlayerHealth.frame:SetScript("OnEvent", BBFrame_EventConsumer(PlayerHealth))
-    PlayerPower.frame:SetScript("OnEvent", BBFrame_EventConsumer(PlayerPower))]]--
 end
